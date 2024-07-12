6f3e859a77cb777dc6bd26df6ba6216fc8c53ccd
AshtonF04 <ashtonfranklin772@gmail.com>
2024-07-11 23:01:33 -0400

    Connected server to mongodb database.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-// require dotenv module to access environment variables
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+
 const workoutRoutes = require('./routes/workout')
 
 // create express ap
@@ -18,8 +19,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+        console.log('listening on port ', process.env.PORT)
+        })
+    })
+    .catch((err) => console.log(err))
+
+
 
